8a3305c79123fa4f8823bd3bf64aaad40ecd366a
byIx <40878600+byIx@users.noreply.github.com>
2022-06-22 04:12:49 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1655834400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"1.0.0", "longversion":10000, "released":1655834400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2680-k4-profile-visitors-count\/" }
