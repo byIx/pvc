0da702c697b4fb648df2481b7c463815c51e9c17
byIx <40878600+byIx@users.noreply.github.com>
2022-07-11 06:28:17 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1655834400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2680-k4-profile-visitors-count-changer\/" }
+{ "version":"1.1.0", "longversion":11000, "released":1657393200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2680-k4-profile-visitors-count-changer\/" }
